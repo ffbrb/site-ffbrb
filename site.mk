f6231fe373bec65974896b750cf6729078763c0f
SebastianMahlow <mail@sebastianmahlow.de>
2016-11-19 02:43:17 +0100

    first l2tp

--- site.mk
+++ site.mk
@@ -8,10 +8,10 @@ GLUON_SITE_PACKAGES := \
   gluon-config-mode-core \
   gluon-config-mode-geo-location \
   gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
+  gluon-config-mode-tunneldigger \
   gluon-ebtables-filter-multicast \
   gluon-ebtables-filter-ra-dhcp \
-  gluon-ffbrb-autokey \
+  gluon-ebtables-filter-roguenets \
   gluon-luci-theme \
   gluon-luci-admin \
   gluon-luci-autoupdater \
@@ -19,19 +19,23 @@ GLUON_SITE_PACKAGES := \
   gluon-luci-private-wifi \
   gluon-luci-wifi-config \
   gluon-next-node \
-  gluon-mesh-vpn-fastd \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
   gluon-radvd \
   gluon-setup-mode \
   gluon-status-page \
+  gluon-migrate-vpn \
   haveged \
   iptables \
   iwinfo
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= 2016.2.1~$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
+GLUON_RELEASE ?= 2016.2.1~l2tp-$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
 
 # Languages to include
 GLUON_LANGS ?= en de
+
+GLUON_ATH10K_MESH ?= ibss
